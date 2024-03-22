1b79312e3f0b0bd4ce603350604df1b9084da042
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-03-21 21:54:46 -0300

    fix: adjust scroll area on default layout

--- src/pages/_layouts/default-layout.tsx
+++ src/pages/_layouts/default-layout.tsx
@@ -5,10 +5,12 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 
 export function DefaultLayout() {
   return (
-    <div className="max-w-screen flex h-screen flex-col items-center gap-2">
+    <div className="max-w-screen flex h-screen flex-col items-center justify-center gap-2">
       <Header />
-      <ScrollArea className="flex h-full w-full max-w-6xl flex-col items-center">
-        <Outlet />
+      <ScrollArea className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex w-full items-center justify-center">
+          <Outlet />
+        </div>
       </ScrollArea>
     </div>
   )
